ae22b91c603c0d8bad2454ff4ecd0986b3c0e9d8
Don Corley <don@donandann.com>
2022-08-12 18:50:01 -0700

    Upgrade log4j to 2.18.0

--- examples/seismic/run.sh
+++ examples/seismic/run.sh
@@ -1,3 +1,3 @@
-CLASSPATH=../../lib/jibx-ws.jar:../../lib/commons-logging.jar:../../lib/log4j.jar:$JIBX_HOME/lib/jibx-run.jar:$JIBX_HOME/lib/xpp3.jar:$JIBX_HOME/lib/jibx-extras.jar
+CLASSPATH=../../lib/jibx-ws.jar:../../lib/commons-logging.jar:../../lib/log4j-api.jar:../../lib/log4j-1.2-api.jar:../../lib/log4j-core.jar:$JIBX_HOME/lib/jibx-run.jar:$JIBX_HOME/lib/xpp3.jar:$JIBX_HOME/lib/jibx-extras.jar
 java -cp build/classes:$CLASSPATH com.sosnoski.seismic.client.BindClient $@
  
